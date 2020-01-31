ff1aa9eccafcb0bfc528833a309b378c2ad386d3
Jieun Kang <jieunkang101@gmail.com>
2020-01-31 12:57:23 -0800

    add event listener for modal

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,25 @@ function Pizza(size) {
   this.size = size,
   this.toppings = []
 }
-var cost = 10;
 
 Pizza.prototype.addTopping = function() {
-  for (var i=0; i<toppings.length; i++) {
-    var toppings = ["Cheese", "Sausage", "Olives", "Mushrooms", "Sun-Dried Tomatoes"];
-    var addTopping = [];
-    addTopping.push(toppings[i]);
-  }
-  console.log(addTopping);
+  $('input[type:"checkbox"]').click(function() {
+    if($(this).prop("checked") == true) {
+      toppings.push(inputtedToppings);
+    }
+  });
 }
+
+// Pizza.prototype.addTopping = function() {
+//   for (var i=0; i<toppings.length; i++) {
+//     var toppings = ["Cheese", "Sausage", "Olives", "Mushrooms", "Sun-Dried Tomatoes"];
+//     var addTopping = [];
+//     addTopping.push(toppings[i]);
+//   }
+//   this.customToppings.push(addTopping);
+// }
+
+var cost = 10;
 
 Pizza.prototype.costOfPizza = function() {
   if (this.size === "Family") {
@@ -22,31 +31,37 @@ Pizza.prototype.costOfPizza = function() {
   } else {
     this.cost; //check!
   }
-  if (this.toppings) {
-    for (var i=0; i<toppings.length; i++) {
-      this.cost += (i+1) * 2
-    }
-  }
+  // if (this.toppings) {
+  //   for (var i=0; i<toppings.length; i++) {
+  //     this.cost += (i+1) * 2
+  //   }
+  // }
   return "$" + cost;
 }
 
-function Topping() {
-  this.topping = topping;
-} 
+
 
 
 // =========User Interface Logic===========
 var pizza = new Pizza();
 
-
-$(document).ready(function() {
-  $("form#frmGetStarted").submit(function(event) {
-    event.preventDefault();
-    $("#myModal").show();
+function addToCartListeners() {
+  $("div.modal-footer").on("click", "button#addPizzaToCart", function() {
     var inputtedSize = $("input:radio[name=pizzaSize]:checked").val();
+    console.log(inputtedSize);
     var inputtedToppings = $("input:checkbox[name=pizzaToppings]:checked").val();
+    console.log(inputtedToppings);
     var newOrder = new Pizza (inputtedSize, inputtedToppings);
     pizza.costOfPizza(newOrder);
     console.log (pizza);
+  });
+}
+
+
+$(document).ready(function() {
+  addToCartListeners();
+  $("form#frmGetStarted").submit(function(event) {
+    event.preventDefault();
+    $("#myModal").show();
   });
 });
